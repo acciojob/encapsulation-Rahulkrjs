47797413e2509001b4dfca67a414b0a9cb1498a4
Rahul Kumar Jaiswal <rahulkumar8533205@gmail.com>
2025-01-15 22:43:02 +0530

    Assignment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Rahul";
+        //System.out.println(obj.name);
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Rahul");
+
+        System.out.println(obj.getName());
+    }
   
 }
